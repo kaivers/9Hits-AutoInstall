aa5568ae4a07b5e8ebd25893a152ee133cc97575
kaivers <37543973+kaivers@users.noreply.github.com>
2021-08-20 05:42:37 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /root/9Hits/
 /root/kill.sh
-wget https://rs.9hits.com/9hviewer/9hviewer-linux-x64.tar.bz2
-cd /root/9Hits/9HitsViewer_x64/
+wget https://www.dropbox.com/s/usku8rcyysk9664/9hits-linux-x64-2.5.2.tar.bz2?dl=1
+cd /root/9hits-linux-x64-2.5.2.tar.bz2?dl=1/
 unzip 9h-patch-linux-x64.zip
 cd /root/9Hits/
 rm 9h-patch-linux-x64.zip
